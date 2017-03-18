9589e68781668fdf054a0cd23c0132bcf0b5bc11
Emmanuel Faboya <efaboya@hotmail.co.uk>
2017-03-18 21:22:00 +0000

    story controller

--- app/models/story.rb
+++ app/models/story.rb
@@ -1,2 +1,5 @@
 class Story < ActiveRecord::Base
+	acts_as_votable
+	belongs_to :user
+	has_many :comments
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170318210212) do
+ActiveRecord::Schema.define(version: 20170318212032) do
 
   create_table "stories", force: :cascade do |t|
     t.string   "title"
@@ -19,7 +19,10 @@ ActiveRecord::Schema.define(version: 20170318210212) do
     t.text     "summary"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer  "user_id"
   end
+
+  add_index "stories", ["user_id"], name: "index_stories_on_user_id"
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
